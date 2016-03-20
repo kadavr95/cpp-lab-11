9692768585c0b201d6cf26c8669c3b4ec90818a9
Yaskovich Dmitry <kadavr95@outlook.com>
2016-03-21 00:11:00 +0300

    -

--- Lab11.cpp
+++ Lab11.cpp
@@ -1,39 +1,41 @@
 #include <iostream.h>
 #include <conio.h>
 #include <math.h>
+#include <iomanip.h>
 
-int AccuracyInput(float *);
-int IntegrateFunction(float, float *, float);
-int WeddleRule(float *,float, float, float);
-void RowOutput(float, float);
-float FunctionValue(float);
+int AccuracyInput(long double *);
+int IntegrateFunction(long double, long double *, long double);
+int WeddleRule(long double *,long double, long double, long double);
+void RowOutput(long double, long double, long double *);
+long double FunctionValue(long double);
 
 const DataPoints=4;
 
 void main()
 {
-	float Counter, Result=0, Accuracy;
-	float ArrayTableResults[DataPoints]={0.492344,0.779893,0.445261,0.488253};
-	cout<<"hello";
+	long double Counter, Result=0, Accuracy;
+	long double ArrayTableResults[DataPoints]={0.4923442258714464,0.7798934003768228,0.4452611760398215,0.4882534060753408};
 	AccuracyInput(&Accuracy);
-	cout<<Accuracy;
-	for (Counter = 0.5; Counter <= 2.0; Counter+=0.5)
+	cout<<"\n------------------------------------------------------------------------------------------------------------------------";
+	cout<<" Lower Limit |"<<" Upper Limit |"<<" Result \n";
+	cout<<"------------------------------------------------------------------------------------------------------------------------";
+	for (Counter = 30.0; Counter <= 30.0; Counter+=0.5)
 	{
 		IntegrateFunction(Counter, &Result, Accuracy);
-		RowOutput(Counter, Result);
+		RowOutput(Counter, Result, ArrayTableResults);
 	}
 	getch();
 }
 
-int AccuracyInput(float *Accuracy)
+int AccuracyInput(long double *Accuracy)
 {
-	cout<<"Enter accuracy";
+	cout<<"Enter accuracy: ";
 	cin>>*Accuracy;
 }
 
-int IntegrateFunction(float Counter, float *Result, float Accuracy)
+int IntegrateFunction(long double Counter, long double *Result, long double Accuracy)
 {
-	float PreviousResult, LowerLimit, UpperLimit;
+	long double PreviousResult, LowerLimit, UpperLimit;
 	int SegmentCounter=1;
 	LowerLimit=0;
 	UpperLimit=Counter;
@@ -44,15 +46,13 @@ int IntegrateFunction(float Counter, float *Result, float Accuracy)
 		PreviousResult=*Result;
 		*Result=0;
 		WeddleRule(Result, SegmentCounter, LowerLimit, UpperLimit);
-		cout<<fabs(*Result-PreviousResult);
     }
 	while(fabs(*Result-PreviousResult)>Accuracy);
-	cout<<"kek";
 }
 
-int WeddleRule(float *Result, float SegmentCounter, float LowerLimitGlobal, float UpperLimitGlobal)
+int WeddleRule(long double *Result, long double SegmentCounter, long double LowerLimitGlobal, long double UpperLimitGlobal)
 {
-	float LowerLimit, UpperLimit, ElementarySegmentLength, SubsegmentLength, SegmentsQuantity;
+	long double LowerLimit, UpperLimit, ElementarySegmentLength, SubsegmentLength, SegmentsQuantity;
 	int Counter;
 	for (Counter = 1; Counter <= SegmentCounter; Counter++)
 	{
@@ -62,19 +62,27 @@ int WeddleRule(float *Result, float SegmentCounter, float LowerLimitGlobal, floa
 		//SubsegmentLength=(UpperLimit-LowerLimit)/SegmentCounter;
 		SubsegmentLength=(UpperLimit-LowerLimit);
 		ElementarySegmentLength=SubsegmentLength/6;
+		cout<<FunctionValue(LowerLimit)<<"\n";
+		cout<<FunctionValue(LowerLimit+ElementarySegmentLength)<<"\n";
+		cout<<FunctionValue(LowerLimit+2*ElementarySegmentLength)<<"\n";
+		cout<<FunctionValue(LowerLimit+3*ElementarySegmentLength)<<"\n";
+		cout<<FunctionValue(LowerLimit+4*ElementarySegmentLength)<<"\n";
+		cout<<FunctionValue(LowerLimit+5*ElementarySegmentLength)<<"\n";
+		cout<<FunctionValue(LowerLimit+6*ElementarySegmentLength)<<"\n";
+		cout<<"\n";
 		*Result+=3*ElementarySegmentLength/10*(FunctionValue(LowerLimit)+5*FunctionValue(LowerLimit+ElementarySegmentLength)+FunctionValue(LowerLimit+2*ElementarySegmentLength)+6*FunctionValue(LowerLimit+3*ElementarySegmentLength)+FunctionValue(LowerLimit+4*ElementarySegmentLength)+5*FunctionValue(LowerLimit+5*ElementarySegmentLength)+FunctionValue(LowerLimit+6*ElementarySegmentLength));
 	}
-
-	cout<<"azaza";
 }
 
-float FunctionValue(float x)
+long double FunctionValue(long double x)
 {
 	return cos(M_PI*x*x/2);
 }
 
-void RowOutput(float Counter, float Result)
+void RowOutput(long double Counter, long double Result, long double *ArrayTableResults)
 {
-	cout<<"mde";
-	cout<<Result;
+	//cout<<fixed<<setprecision(1)<<"     "<<0.0<<"     |     "<<Counter<<"     | "<<setprecision(15)<<Result<<" \n"<<"               Table Result: "<<ArrayTableResults[int(Counter*2-1)]<<"\n";
+	cout<<fixed<<setprecision(1)<<"     "<<0.0<<"     |     "<<Counter<<"     | "<<setprecision(15)<<Result<<" \n";
+	cout<<"------------------------------------------------------------------------------------------------------------------------";
+
 }
